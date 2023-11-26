0cf5f723bb999301b471b4722b880489d6807677
Arbaaz Shafiq <arbaazshafiq@gmail.com>
2023-11-26 17:01:52 +0530

    done: add commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
+#![allow(dead_code)]
 mod util;
 
-use std::{fs, path::PathBuf};
+use std::{fs, path::PathBuf, time};
 
 use anyhow::Result;
 
@@ -9,6 +10,8 @@ struct Git {
     dir: PathBuf,
     // current branch
     branch: String,
+    user: String,
+    email: String,
 }
 
 impl Git {
@@ -27,6 +30,60 @@ impl Git {
         fs::write(obj, bz)?;
 
         Ok(Hash(sum))
+    }
+
+    fn add_blob(&self, data: &[u8]) -> Result<Hash> {
+        self.write("blob", data)
+    }
+
+    fn add_tree(&self, filename: &str, filedata: &[u8]) -> Result<Hash> {
+        let hash = self.add_blob(filedata)?;
+        let mut content = format!("100644 {filename}\x00").into_bytes();
+        content.extend(&hash.0);
+        self.write("tree", &content)
+    }
+
+    fn add_commit(
+        &self,
+        filename: &str,
+        data: &[u8],
+        parent_hash: Option<Hash>,
+        msg: &str,
+    ) -> Result<Hash> {
+        let hash = self.add_tree(filename, data)?;
+
+        let parent = if let Some(p) = parent_hash {
+            format!("parent {}\n", p.to_string())
+        } else {
+            String::new()
+        };
+
+        let t = time::UNIX_EPOCH.elapsed()?.as_secs();
+
+        let content = format!(
+            "tree {}\n{}author {} <{}> {} +0000\ncommitter {} <{}> {} +0000\n{}\n",
+            hash.to_string(),
+            parent,
+            &self.user,
+            &self.email,
+            t,
+            &self.user,
+            &self.email,
+            t,
+            msg
+        )
+        .into_bytes();
+
+        let b = self.write("commit", &content)?;
+        self.set_head(&b)?;
+
+        Ok(b)
+    }
+
+    fn set_head(&self, hash: &Hash) -> Result<()> {
+        let filepath = self.dir.join("ref").join("heads").join(&self.branch);
+        fs::write(filepath, hash.to_string().into_bytes())?;
+        Ok(())
     }
 }
 
